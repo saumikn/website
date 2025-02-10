0b1d13bd0a88f55a08bc6e011423c74c7b6b1d88
Saumik Narayanan <saumik.narayanan@outlook.com>
2025-02-10 11:58:35 -0600

    First commit, just copied all of joyofcode tutorial code

---
+++ src/lib/config.ts
@@ -0,0 +1,5 @@
+import { dev } from '$app/environment';
+
+export const title = 'Saumik Narayanan';
+export const description = 'SvelteKit blog for poets';
+export const url = dev ? 'http://localhost:5173/' : 'https://saumikn.com/';
